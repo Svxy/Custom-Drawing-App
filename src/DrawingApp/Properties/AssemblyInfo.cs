fb2c17d303f77bc77cba358ed590a8ed472e986a
c̶u̶t̶i̶e̶y̶ツ゚ <93295911+Svxy@users.noreply.github.com>
2022-03-14 19:22:20 -0400

    Add files via upload

---
+++ src/DrawingApp/Properties/AssemblyInfo.cs
@@ -0,0 +1,24 @@
+﻿using System.Reflection;
+using System.Resources;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Windows;
+
+[assembly: AssemblyTitle("TnyavnTo's Drawing App")]
+[assembly: AssemblyDescription("A custom drawing app designed and coded in Visual Studio 2022 by TnyavnTo")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("S&A Organics Co.")]
+[assembly: AssemblyProduct("Drawing App")]
+[assembly: AssemblyCopyright("Copyright © 2022 by TnyavnTo @ S&A Organics Co.")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: ThemeInfo(
+    ResourceDictionaryLocation.None, 
+    ResourceDictionaryLocation.SourceAssembly 
+)]
+
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
