f4ca979d409de5dfa9e8a8132277cd0ed5ed263d
c̶u̶t̶i̶e̶y̶ツ゚ <93295911+Svxy@users.noreply.github.com>
2022-03-14 19:21:35 -0400

    Add files via upload

---
+++ src/DrawingApp/MainWindow.xaml.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Windows;
+using System.IO;
+using System.Windows.Media;
+using System.Windows.Controls;
+using System.Windows.Ink;
+using System.Windows.Media.Imaging;
+using Microsoft.Win32;
+
+namespace WpfApp1
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            SelfRef = this;
+            InitializeComponent();
+        }
+
+        public static MainWindow SelfRef    // link on this private components to other classes
+        {
+            get; set;
+        }
+
+        private void AboutOpen_Enter(object sender, RoutedEventArgs e)
+        {
+            ToolTip tOpen = new ToolTip();
+            ItemOpen.ToolTip = tOpen;
+            tOpen.Content = "Open file and load to my editor!";
+        }
+
+        private void AboutSave_Enter(object sender, RoutedEventArgs e)
+        {
+            ToolTip tSave = new ToolTip();
+            ItemSave.ToolTip = tSave;
+            tSave.Content = "Save file to initial directory without bitmap coding for further opening in my editor.";
+        }
+
+        private void AboutExport_Enter(object sender, RoutedEventArgs e)
+        {
+            ToolTip tExport = new ToolTip();
+            ItemExport.ToolTip = tExport;
+            tExport.Content = "Export file to open in Windows. This will not be avaliable to open in my editor.";
+        }        
+
+        private void EditClear_Click(object sender, RoutedEventArgs e)
+        {
+            inkCanvas1.Strokes.Clear();
+        }
+
+        private void ItemExit_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }       
+
+        private void ToolsOpen_Click(object sender, RoutedEventArgs e)
+        {
+            Palette obj = new Palette();
+            obj.Show();
+        }
+
+        private void MessageBoxForAbout(object sender, RoutedEventArgs e)
+        {
+            _ = MessageBox.Show("Hello and thank you for downloading my drawing app!\nThis is a recreation based on an open source project, built in Visual Studio 2022 Community.\n\nThere are some decent functions here aswell as an advanced color pallete to let your creativity flow!\n\nNOTE: Resizing the window will leave any current drawing in its place, resulting in a larger canvas.\n\nSIDENOTE: Saving the file is primarily meant for projects in progress and can in most cases corrupt the file if trying to open it normally. Use the export button when the project is complete to get a working image in 4 different formats of your choice!\n\nCopyright © 2022 by TnyavnTo @ S&A Organics Co.", "About My Drawing App", MessageBoxButton.OK);
+
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ src/DrawingApp/MainWindow_Files.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Windows;
+using System.IO;
+using System.Windows.Media;
+using System.Windows.Controls;
+using System.Windows.Ink;
+using System.Windows.Media.Imaging;
+using Microsoft.Win32;
+using System.Runtime;
+
+namespace WpfApp1
+{
+    public partial class MainWindow 
+    {
+        FileStream myfileStream = null;
+        private Stream myStream = null;
+        public String imgBuf = @"WIP_USE_EXPORT_FOR_WORKING_IMAGE.png";   //the path to save (not to preview)  
+
+        OpenFileDialog openFileDialog1 = new OpenFileDialog
+        {
+            InitialDirectory = Directory.GetCurrentDirectory(),
+            Filter = "All files (*.*)|*.*|PNG (*.png)|*.png",
+            FilterIndex = 2,
+            RestoreDirectory = true
+        };
+
+        SaveFileDialog saveFileDialog1 = new SaveFileDialog
+        {
+            InitialDirectory = Directory.GetCurrentDirectory(),
+            Filter = "JPEG (*.jpg)|*.jpg|BMP (*.bmp*)|*.bmp|PNG (*.png)|*.png|GIF (*.gif)|*.gif",
+            FilterIndex = 2,
+            RestoreDirectory = true
+        };
+
+        private void ItemOpen_Click(object sender, RoutedEventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == true)
+            {
+                try
+                {
+                    myStream = openFileDialog1.OpenFile();
+                    StrokeCollection strokes = new StrokeCollection(myStream);
+                    inkCanvas1.Strokes = strokes;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: Unable to upload file. Original error: " + ex.Message);
+                }
+                finally
+                {
+                    myStream.Close();
+                }
+            }
+        }
+
+        private void ItemSave_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                myfileStream = new FileStream(imgBuf, FileMode.Create);
+                inkCanvas1.Strokes.Save(myfileStream);
+                MessageBox.Show("Picture was successfully saved!");
+            }
+            catch
+            {
+                MessageBox.Show("Unable to save file. Check the path.");
+            }
+            finally
+            {
+                myfileStream.Close();
+            }
+        }
+
+        private void ItemExport_Click(object sender, RoutedEventArgs e)
+        {
+            if (saveFileDialog1.ShowDialog() == true)
+            {
+                try
+                {
+                    myfileStream = new FileStream(saveFileDialog1.FileName, FileMode.Create);
+                    RenderTargetBitmap rtb = new RenderTargetBitmap((int)inkCanvas1.ActualWidth, (int)inkCanvas1.ActualHeight, 96d, 96d, PixelFormats.Default);
+                    rtb.Render(inkCanvas1);
+
+                    PngBitmapEncoder pngEnc = new PngBitmapEncoder();
+                    pngEnc.Frames.Add(BitmapFrame.Create(rtb));
+                    pngEnc.Save(myfileStream);
+                    MessageBox.Show("Picture was successfully exported!");
+                }
+                catch
+                {
+                    MessageBox.Show("Unable to export file. Check the path.");
+                }
+                finally
+                {
+                    myfileStream.Close();
+                }
+            }
+        }
+    }
+}
